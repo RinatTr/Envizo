3800c54021198ad9f0b2463c4cbbacb292db4caa
Michell Tejada <michelltejada@pursuit.com>
2019-04-08 19:17:29 -0400

    wip

--- backend/routes/communities.js
+++ backend/routes/communities.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllCommunities, addCommunity  } = require('../db/queries/subscriptionQueries.js')
+const { getAllCommunities, addCommunity  } = require('../db/queries/communitiesQueries.js')
 
 router.get('/', getAllCommunities);
 router.post('/new', addCommunity);
