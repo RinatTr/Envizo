54df67ec1492b16d67a7c4d414b77b1cf650921e
RinatTr <rinatreg@gmail.com>
2019-04-08 12:36:21 -0400

    faker initial

--- backend/db/faker_seed_part1.js
+++ backend/db/faker_seed_part1.js
@@ -1,29 +1,72 @@
 const { db } = require('./index.js')
 const faker = require('faker')
 
-let community = ['Manhattan','Queens','Bronx','Brooklyn','Staten Island'];
+let communities = ['Manhattan','Queens','Bronx','Brooklyn','Staten Island'];
+
+for (let i = 0; i < communities.length; i++) {
+  let name = communities[i]
+  let str = `('${name}')`
+  communities.push(str)
+}
+
+let users = []
 
 for (let i = 0; i < 25; i++) {
-  //creates a string which can be put after VALUES
   let username = faker.name.findName();
+  let password_digest = faker.internet.password();
   let email = faker.internet.email();
-  let str = `('${username}', '${email}')`
+  let community_id = Math.floor(Math.random() * 5) + 1;
+  let avatar_img = `https://api.adorable.io/avatars/200/${username}`
+  let str = `('${username}', '${password_digest}', '${email}' ,${community_id}, '${avatar_img}')`
   users.push(str)
 }
 
-let photos = [];
+let goals = [];
 
-for (let i = 0; i < 50; i++) {
-  //creates a string which can be put after VALUES
-  let user_id = Math.floor(Math.random() * 25) + 1;
-  let url = faker.image.imageUrl();
-  let str = `(${user_id}, '${url}')`;
-  photos.push(str)
+for (let i = 0; i < communities.length; i++) {
+  let description = "Upload a picture of your reusable grocery bag after you are back from grocery shopping."
+  let title = "Reusable Grocery Bag"
+  let community_id = i+1;
+  let target_value = 2000
+  let completed = false
+  let str = `(${description}, '${title}', ${community_id}, ${target_value}, ${completed})`;
+  goals.push(str)
 }
 
-users = users.join(", ")
-photos = photos.join(", ")
+let subscriptions = [];
 
-db.none("INSERT INTO users(username, email) VALUES " + users + ";")
-.then(()=> {"INSERT INTO photos(user_id, url) VALUES " + photos + ";"})
-.catch(err => console.log(err)); //have to chain the seedings, since it's a promise (it takes time to seed the database.)
+for (let i = 0; i < 25; i++) {
+  let goal_id = Math.floor(Math.random() * 5) + 1;
+  let user_id = Math.floor(Math.random() * 25) + 1;
+  let str = `('${goal_id}, ${user_id})`
+  subscriptions.push(str)
+}
+
+let submissions = [];
+
+for (let i = 0; i < 40; i++) {
+  let img_url = `https://www.pppmi.com/wp-content/gallery/Retail-Non-Woven-Bags/whole-foods-grocery-bag.jpg`
+  let subscriptions_id = Math.floor(Math.random() * 25) + 1;
+  let str = `('${img_url}, ${subscriptions_id})`
+  submissions.push(str)
+}
+
+let activity = ['joined','subscribed','uploaded','milestone'];
+
+for (let i = 0; i < 40; i++) {
+  let type = activity[Math.floor(Math.random() * 4) + 1]
+  let activity_init_id = Math.floor(Math.random() * 25) + 1;
+  let str = `('${type}', ${activity_init_id})`
+  activity.push(str)
+}
+
+communities = communities.join(", ")
+users = users.join(", ")
+goals = goals.join(", ")
+subscriptions = subscriptions.join(", ")
+submissions = submissions.join(", ")
+activity = activity.join(", ")
+
+// db.none("INSERT INTO users(username, email) VALUES " + users + ";")
+// .then(()=> {"INSERT INTO photos(user_id, url) VALUES " + photos + ";"})
+// .catch(err => console.log(err)); //have to chain the seedings, since it's a promise (it takes time to seed the database.)
