eb8b9f04a6887042a498876eeab37e5e733f21a5
RinatTr <rinatreg@gmail.com>
2019-04-09 15:29:20 -0400

    update create submission route

--- backend/routes/submission.js
+++ backend/routes/submission.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const {
   getAllSubmissions,
   getAllSubmissionForSingleGoal,
-  createNewSubmission, 
+  createNewSubmission,
   deleteSubmission
 } = require('../db/queries/SubmissionQueries');
 
 router.get('/', getAllSubmissions);
 router.get('/:goalId', getAllSubmissionForSingleGoal);
-router.post('/', createNewSubmission);
+router.post('/:user_id', createNewSubmission);
 router.delete('/:submissionId', deleteSubmission);
 
 module.exports = router;
