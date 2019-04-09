3e0424633143414c2033d66ef728bb798aaecf4f
Michell Tejada <michelltejada@pursuit.com>
2019-04-09 15:56:36 -0400

    Added query to get activity for a community

--- backend/routes/communities.js
+++ backend/routes/communities.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllCommunities, addCommunity  } = require('../db/queries/communitiesQueries.js')
+const { getAllCommunities, addCommunity, getAllActivityForACommunity  } = require('../db/queries/communitiesQueries.js')
 
 router.get('/', getAllCommunities);
+router.get('/:id/activity', getAllActivityForACommunity)
 router.post('/new', addCommunity);
 
 module.exports = router
