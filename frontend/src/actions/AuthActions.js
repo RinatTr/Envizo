a861bfe1026a989ec8dbfc66dc715405b9c7a9b9
your name here <your_email_here>
2019-04-11 17:35:42 -0400

    frontendauth is working now

---
+++ frontend/src/actions/AuthActions.js
@@ -0,0 +1,112 @@
+// import * as Util from '../util.js';
+import axios from 'axios'
+import Auth from "../util/Auth.js"
+
+export const NEW_USER = "NEW_USER";
+export const LOG_IN = "LOG_IN";
+export const CURRENT_USER = "CURRENT_USER"
+
+//logs out the user
+export const logout = () => dispatch => {
+  axios
+    .post("/sessions/logout")
+    .then(() => {
+      Auth.deauthenticateUser();
+    })
+    .then(() => {
+
+      checkAuthenticateStatus();
+    });
+}
+//checks if the user has the same id on the frontend as the user on the backend
+export const checkAuthenticateStatus = () => dispatch => {
+
+
+  axios
+    .get("/sessions/isLoggedIn").then(user => {
+
+    if (user.data.id === +Auth.getToken()){
+
+
+      dispatch({
+        type:LOG_IN,
+        payload:user.data
+      })
+
+      dispatch(loadCurrent())
+
+    } else {
+      if (user.data.id) {
+        logout()
+      } else {
+        Auth.deauthenticateUser();
+      }
+    }
+  })
+
+  ;
+}
+export const newUser = newUserData => dispatch => {
+  console.log(newUserData);
+  axios
+  .post("/sessions/new", newUserData)
+    .then(res => {
+      dispatch({
+        type:NEW_USER,
+        user:res
+      })
+
+      axios
+      .post("/sessions/login",{username:newUserData.username, password:newUserData.password})
+        .then(res => {
+          Auth.authenticateUser(res.data.id);
+          dispatch({
+            type:LOG_IN,
+            payload:res.data.id
+          })
+        })
+      .then(()=> {
+        console.log('in new User');
+        checkAuthenticateStatus()
+      })
+    })
+
+}
+
+export const logIn = logInData => dispatch => {
+  axios
+  .post("/sessions/login", logInData)
+    .then(res => {
+      console.log('res of login', res.data);
+      Auth.authenticateUser(res.data.id);
+      dispatch({
+        type:LOG_IN,
+        payload:res.data
+      })
+
+    })
+    .then(()=> {
+      console.log('check auth at login');
+      checkAuthenticateStatus();
+
+    })
+    .catch(err=> {
+      console.log(err);
+    })
+}
+
+
+export const loadCurrent = () => dispatch => {
+  axios
+  .get(`/users/${+Auth.getToken()}`)
+    .then(res => {
+      dispatch({
+        type:CURRENT_USER,
+        payload:res.data.user
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+}
--- frontend/src/reducers/AuthReducer.js
+++ frontend/src/reducers/AuthReducer.js
@@ -1,7 +1,42 @@
-// import { actions.. } from '../actions/authactions'
+import {NEW_USER, LOG_IN, CURRENT_USER,} from '../actions/AuthActions';
+import Auth  from '../util/Auth.js'
 
-const AuthReducer = (oldState = {}, action) => {
-  return {};
+const initialState = {
+  user: {},
+  isLoggedIn: null,
+  userId: null,
+  username:null,
+  email:"",
+  currentUser: {
+  img_url:"https://bloximages.newyork1.vip.townnews.com/kentwired.com/content/tncms/assets/v3/editorial/f/95/f95384b0-edfc-59b2-8b1e-4a6f2c3bc61c/52c5029f44428.image.jpg"
+  }
 }
 
-export default AuthReducer;
+const AuthReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case NEW_USER:
+    //user refers to the payload
+      return {
+        ...state,
+        user :action.user
+      }
+    case LOG_IN:
+
+      return {
+        ...state,
+        userId: +Auth.getToken(),
+        username:action.payload.id,
+        isLoggedIn:Auth.isUserAuthenticated()
+      }
+    case CURRENT_USER:
+      return {
+        ...state,
+        currentUser:action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export default AuthReducer
