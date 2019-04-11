37acf45b220f0705c7050c08a348ba3c1c35a028
Michell Tejada <michelltejada@pursuit.com>
2019-04-11 16:06:00 -0400

    Added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomeContainer from './containers/HomeContainer';
+import Navbar from './Navbar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Switch>
           <Route path='/' component={HomeContainer}/>
         </Switch>
