4a5135a378bc3a0d146b415289f251dd45172d74
RinatTr <rinatreg@gmail.com>
2019-04-11 17:38:27 -0400

    init d3 and component, pre-adapting to react

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import VisualDisplay from './VisualDisplay'
 class Home extends Component {
   constructor() {
     super()
---
+++ frontend/src/components/VisualDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import visual from '../util/visual.js'
+import d3 from 'd3';
+// import info from "./info.json";
+// import "./styles.css";
+// import "./visual.js";
+
+
+// export const VisualDisplay = () => {
+//
+// }
