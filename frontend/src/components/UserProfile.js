5ca1ef8b5fa3c971f42fad27c5fd4f7ff36eeab2
godsby <gg4biz@gmail.com>
2019-04-19 12:29:26 -0400

    goalsList ready for UserProfile

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -5,14 +5,30 @@ import { Link } from 'react-router-dom'
 
 class UserProfile extends Component {
 
+  state = {
+    goalsForUser: []
+  }
+
   componentDidMount() {
     this.props.fetchAllGoals();
     this.props.checkAuthenticateStatus();
     this.props.fetchAllUsers();
+    this.getSubscriptionsForAUser();
+  }
+
+  getSubscriptionsForAUser = () => {
+    axios.get(`/subscriptions/user/${this.props.match.params.id}`)
+    .then(res => {
+      this.setState({
+        goalsForUser: res.data.subscripUser
+      })
+    })
+    .catch(err => Error)
   }
   
   render() {
     const { users, goals } = this.props;
+    const { goalsForUser } = this.state;
     console.log(goals)
     let imgUrl;
     const usersInfo = users.find(user => {
@@ -25,41 +41,42 @@ class UserProfile extends Component {
       imgUrl = ''
     }
 
-    // const goalsList = goals.find(goal => {
-    //   return 
-    // })
+
+    const goalsList = goalsForUser.length ? goalsForUser.map(goal => {
+      return (
+        <>
+         <CollectionItem>
+            { goal.title }
+            <a href="javascript:void(0)" className="secondary-content">
+              <Icon>
+                send
+              </Icon>
+            </a>
+          </CollectionItem>
+        </>
+      )
+    }) : <p>No subscriptions yet...</p>
 
     return (
       <>
         <Row className='center'>
-        {/* User side */}
+
+          {/* User side */}
           <Col l={4} className="teal push-l1 black-text">
+
             <Collection className='avatar'>
               <CollectionItem> 
                 <img src={imgUrl} alt="" className="circle"></img>
                 <div><Link to='/community/:id'>Community Name</Link></div>
               </CollectionItem>
             </Collection>
+
             <Collection>
-              <CollectionItem>
-              {/* <img src="https://materializecss.com/images/yuna.jpg" alt="" className="circle" /> */}
-              Alvin
-              <a href="javascript:void(0)" className="secondary-content">
-              <Icon>
-              send
-              </Icon>
-              </a>
-              </CollectionItem>
-              <CollectionItem>
-              Alvin
-              <a href="javascript:void(0)" className="secondary-content">
-              <Icon>
-              send
-              </Icon>
-              </a>
-              </CollectionItem>
+              { goalsList }
             </Collection>
+
           </Col>
+          
           {/* Activities side */}
           <Col l={5} className="teal offset-l2 black-text">
             <Collection>
