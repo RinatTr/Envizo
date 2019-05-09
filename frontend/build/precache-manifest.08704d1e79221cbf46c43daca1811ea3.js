96d9d0e217d38abefab5574c1763739fbbf20a9e
RinatTr <rinatreg@gmail.com>
2019-05-09 16:29:45 -0400

    add build dir to git

---
+++ frontend/build/precache-manifest.08704d1e79221cbf46c43daca1811ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "479f1bc72d6df2b72476",
+    "url": "/static/css/main.e90ec511.chunk.css"
+  },
+  {
+    "revision": "479f1bc72d6df2b72476",
+    "url": "/static/js/main.06fe3e54.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5b20e2eb2145fc5dde07",
+    "url": "/static/js/2.f463b539.chunk.js"
+  },
+  {
+    "revision": "98c57d4f6465d15455c647e953e31404",
+    "url": "/static/media/navbarLogo.98c57d4f.png"
+  },
+  {
+    "revision": "6cb10fbab6dd7f2458d351ad948e738a",
+    "url": "/index.html"
+  }
+];
