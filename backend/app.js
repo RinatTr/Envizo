2f3c312e2c4efcde3f2e0b47c3e528ad0889a7e2
RinatTr <rinatreg@gmail.com>
2019-05-09 17:31:13 -0400

    fix

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ app.use('/api/sessions', sessionsRouter);
 // });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 // catch 404 and forward to error handler
