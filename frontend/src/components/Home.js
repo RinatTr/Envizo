c5df69bef7df62f5f08666d714e62a1601d44d02
your name here <your_email_here>
2019-05-13 11:36:26 -0400

    optimized puzzle for mobile and removed dropdown menu from other tabs on home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -35,6 +35,22 @@ class Home extends Component {
     })
 
   }
+  dropDown = () => {
+    return(
+      <div className="container borough-select">
+        <div className ='input-field col s6'>
+          <Select name='borough' onChange={this.handleChange}>
+            <option value='0'>Select Borough</option>
+            <option value='1'>Manhattan</option>
+            <option value='2'>Queens</option>
+            <option value='3'>Bronx</option>
+            <option value='4'>Brooklyn</option>
+            <option value='5'>Staten Island</option>
+          </Select>
+        </div>
+      </div>
+    )
+  }
 
     render() {
     const { subjects } = this.state
@@ -46,18 +62,23 @@ class Home extends Component {
         <>
         <div className="col s6">
           <Tabs onChange={this.tabChange} className=" tab-demo z-depth-1" options={{swipeable: true}}>
+
             <Tab title="Monthly Tonnage" active>
             <div className="container">
               <h4 id='bold'>Take a dive in NYC's trash pile.</h4>
               </div>
                 <div id="demo">
+                <div className='iframe'>
+
                 <VisualDisplay />
+                </div>
                 <div className="container">
                   <p>* Each circle represents Monthly Tonnage for each borough between 2018-2019, according to NYC Open Data API.</p>
                 </div>
                 <h4 className="center" id="bold">OK. How Can I Help?</h4>
               </div>
             </Tab>
+
             <Tab title="Air Pollution">
               <div className="container"><h5>Coming soon ...</h5></div>
             </Tab>
@@ -66,18 +87,7 @@ class Home extends Component {
             </Tab>
           </Tabs>
           </div>
-          <div className="container borough-select">
-            <div className ='input-field col s6'>
-              <Select name='borough' onChange={this.handleChange}>
-                <option value='0'>Select Borough</option>
-                <option value='1'>Manhattan</option>
-                <option value='2'>Queens</option>
-                <option value='3'>Bronx</option>
-                <option value='4'>Brooklyn</option>
-                <option value='5'>Staten Island</option>
-              </Select>
-            </div>
-          </div>
+          {subjects === '00'? this.dropDown():''}
 
           {list}
 
